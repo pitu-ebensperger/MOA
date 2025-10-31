ee5fc508964c0db7666a9bd8b77795e71c67ce53
pitu-ebensperger <pitu.ebensperger@gmail.com>
2025-10-30 23:37:26 -0300

    update import paths

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { App } from './App.jsx'
-import './styles/index.css'
+import { App } from './App'
+import '../styles/global.css'
+import '../styles/tokens.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
