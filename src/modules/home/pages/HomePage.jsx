d8a7c2053a58036dac55bc275b4a08c6ce043fad
Citlali Garcia <citlali.garcia.ponce@gmail.com>
2025-11-02 16:31:54 -0600

    Modificaciones en estructura Home

--- src/modules/home/pages/HomePage.jsx
+++ src/modules/home/pages/HomePage.jsx
@@ -1,17 +1,20 @@
 import { motion } from "motion/react";
-import { HeroSection } from "../components/HeroSection";
-import { ProductsSection } from "../components/ProductsSection";
-import { CategoriesFilter } from "../components/ModernCategories";
-import {ContactSection} from  "../components/ContactSection";
+import HeroSection from "../components/HeroSection";
+import ProductsSection from "../components/ProductsSection";
+import CategoriesFilters from "../components/CategoriesFilters";
+import ContactSection from "../components/ContactSection";
 
 export const Home = () => {
   return (
-    <div>
-    <HeroSection> </HeroSection>
-    <CategoriesFilter> </CategoriesFilter>
-    <ProductsSection> </ProductsSection>
-
-    <ContactSection></ContactSection>
-    </div>
-  )
-}
+    <motion.div
+      initial={{ opacity: 0, y: 20 }} // Empieza invisible y ligeramente desplazado hacia abajo
+      animate={{ opacity: 1, y: 0 }} // Luego aparece y sube a su posición normal
+      transition={{ duration: 0.6 }}  // En 0.6 segundos
+    >
+      <HeroSection />
+      <CategoriesFilters />
+      <ProductsSection />
+      <ContactSection />
+    </motion.div>
+  );
+};
