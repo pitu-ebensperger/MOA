85bd3348cdda50983d565b6b7c92fa3b3be09877
pitu-ebensperger <pitu.ebensperger@gmail.com>
2025-11-01 23:29:33 -0300

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,16 @@
 import '../styles/global.css'
+import '../styles/tokens.css'
+import '../styles/motion.css'
+
 import { Routes, Route } from 'react-router-dom'
 import { Navbar } from '../components/layout/Navbar.jsx'
 import { Footer } from '../components/layout/Footer.jsx'
-import { Home as HomePage } from '../modules/home/pages/HomePage.jsx'
-import { Cart as CartPage } from '../modules/cart/pages/CartPage.jsx'
-import { Checkout as CheckoutPage } from '../modules/cart/pages/CheckoutPage.jsx'
+import { HomePage } from '../modules/home/pages/HomePage.jsx'
+import { CartPage } from '../modules/cart/pages/CartPage.jsx'
+import { CheckoutPage } from '../modules/cart/pages/CheckoutPage.jsx'
 import { NotFoundPage } from '../modules/support/pages/NotFoundPage.jsx'
 import LoginPage from '../modules/auth/pages/LoginPage.jsx'
 import RegisterPage from '../modules/auth/pages/RegisterPage.jsx'
-import NewProductPage from '../modules/admin/pages/products/NewProductPage.jsx'
 import { ProductDetailPage } from '../modules/products/pages/ProductDetailPage.jsx'
 import ProductsPage from '../modules/products/pages/ProductsPage.jsx'
 import { ProfilePage } from '../modules/profile/pages/ProfilePage.jsx'
@@ -25,7 +27,6 @@ export const App = () => {
         <Route path='/checkout' element={<CheckoutPage />} />
         <Route path='*' element={<NotFoundPage />} />
         <Route path='/login' element={<LoginPage />} />
-        <Route path='/new-product' element={<NewProductPage />} />
         <Route path='/product-detail' element={<ProductDetailPage />} />
         <Route path='/products' element={<ProductsPage />} />
         <Route path='/profile' element={<ProfilePage />} />
