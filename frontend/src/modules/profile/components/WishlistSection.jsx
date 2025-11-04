7258e60b1dd78c271c6b33df179b7dd1069cdcf5
Nahara Gutiérrez <nahgutie@gmail.com>
2025-11-04 09:04:56 -0300

    feat: user information structure

---
+++ frontend/src/modules/profile/components/WishlistSection.jsx
@@ -0,0 +1,7 @@
+const WishlistSection = () => {
+  return (
+      <h2 className="font-italiana text-2xl text-dark mt-24 mb-10 flex justify-center">Mis Favoritos</h2>
+  )
+}
+
+export default WishlistSection
