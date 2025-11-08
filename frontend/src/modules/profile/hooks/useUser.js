74bf0dcc470634f9d584a0cf006680e21d735fbd
pitu-ebensperger <pitu.ebensperger@gmail.com>
2025-11-08 13:19:13 -0300

    fix/axios-import

--- frontend/src/modules/profile/hooks/useUser.js
+++ frontend/src/modules/profile/hooks/useUser.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import { useAuth } from '../../auth/hooks/useAuth';
 
 export const useUser = () => {
