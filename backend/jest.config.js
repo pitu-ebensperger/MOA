edba4d42c82ee2237099b5be8862ea193eac4cd1
Citlali Garcia <citlali.garcia.ponce@gmail.com>
2025-11-19 22:22:45 -0600

    creacion y realizacion de tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,12 +1,11 @@
 export default {
-  testEnvironment: 'node',
-  transform: {},
-  extensionsToTreatAsEsm: ['.js'],
-  testMatch: ['**/__tests__/**/*.test.js'],
-  coveragePathIgnorePatterns: ['/node_modules/'],
+  testEnvironment: "node",
+  transform: {}, 
+  testMatch: ["**/__tests__/**/*.test.js"],
+  coveragePathIgnorePatterns: ["/node_modules/"],
   collectCoverageFrom: [
-    'src/**/*.js',
-    'routes/**/*.js',
-    '!**/node_modules/**'
+    "src/**/*.js",
+    "routes/**/*.js",
+    "!**/node_modules/**"
   ]
 };
