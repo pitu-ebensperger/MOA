117c02f0efb392376a0745018b649f6be4b0ed4b
Nahara Gutiérrez <nahgutie@gmail.com>
2025-11-26 19:11:43 -0300

    feat: admin

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -2,14 +2,11 @@ import { Router } from "express";
 import { getProducts } from "../src/controllers/productsController.js";
 import { getProductById } from "../src/controllers/productsController.js";
 import { createProduct } from "../src/controllers/productsController.js";
-// import { verifyToken } from "../src/middleware/tokenMiddleware.js";
-// import { deleteProductController } from "../src/controllers/productsController.js";
 
 const router = Router();
 
 router.get("/productos", getProducts);
 router.get("/producto/:slug", getProductById);
 router.post("/productos", createProduct);
-// router.delete("/admin/productos/:id", verifyToken, deleteProductController);
 
 export default router;
